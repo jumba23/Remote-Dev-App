6b3efb8b3d2c16e6caf5856c18df186d7a70ead0
Goran SD <g.cvetic23@gmail.com>
2024-03-19 12:35:27 -0700

    Refactor Container and SearchForm components

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,6 +1,12 @@
-import { useEffect, useState } from "react";
+type SearchFormProps = {
+  searchText: string;
+  setSearchText: (text: string) => void;
+};
 
-export default function SearchForm({ searchText, setSearchText }) {
+export default function SearchForm({
+  searchText,
+  setSearchText,
+}: SearchFormProps) {
   return (
     <form onSubmit={(e) => e.preventDefault()} action="#" className="search">
       <button type="submit">

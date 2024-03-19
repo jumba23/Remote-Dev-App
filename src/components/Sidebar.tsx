d2088b16e79b4ff6b2f96ebbe2d22d91ebdd665f
Goran SD <g.cvetic23@gmail.com>
2024-03-19 12:33:39 -0700

    Refactor Header and Sidebar components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,7 @@
-export default function Sidebar({ children }) {
+export default function Sidebar({ children }: { children: React.ReactNode }) {
   return <div className="sidebar">{children}</div>;
 }
+
+export const SidebarTop = ({ children }: { children: React.ReactNode }) => {
+  return <div className="sidebar__top">{children}</div>;
+};

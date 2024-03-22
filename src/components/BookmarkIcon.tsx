cefc69bcf69a76e7cf62ad346c63b2b20f10e513
Goran SD <g.cvetic23@gmail.com>
2024-03-22 12:15:56 -0700

    Refactor BookmarkIcon component and add id prop to JobItemContent and JobListItem components

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -2,14 +2,26 @@ import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 import { useContext } from "react";
 import { BookmarksContext } from "../contexts/BookmarksContextProvider";
 
-export default function BookmarkIcon() {
-  const context = useContext(BookmarksContext);
+type BookmarkIconProps = {
+  id: number;
+};
 
-  console.log("Context: ", context);
+export default function BookmarkIcon({ id }: BookmarkIconProps) {
+  const { bookmarkedIds, toggleHandleBookmark } = useContext(BookmarksContext);
 
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="" />
+    <button
+      onClick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        toggleHandleBookmark(id);
+      }}
+      className="bookmark-btn"
+    >
+      <BookmarkFilledIcon
+        className={`${bookmarkedIds.includes(id) ? "filled" : ""}`}
+      />
     </button>
   );
 }
--- src/contexts/BookmarksContextProvider.tsx
+++ src/contexts/BookmarksContextProvider.tsx
@@ -2,12 +2,14 @@ import { createContext, useState } from "react";
 
 export const BookmarksContext = createContext(null);
 
-const BookmarksContextProvider = ({ children }) => {
+export const BookmarksContextProvider = ({ children }) => {
   const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+
+  console.log(bookmarkedIds);
 
   const toggleHandleBookmark = (id: number) => {
     if (bookmarkedIds.includes(id)) {
-      setBookmarkedIds((prev) => prev.filter((id) => id !== id));
+      setBookmarkedIds((prev) => prev.filter((item) => item !== id));
     } else {
       setBookmarkedIds((prev) => [...prev, id]);
     }
